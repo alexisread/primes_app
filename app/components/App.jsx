c923c2b76758e6279c794c0c2aff38f5e61798bf
alexisread <alexis.read@gmail.com>
2016-02-02 01:24:36 +0000

    Hook up all the components in App.jsx

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import genNPrimes from './../libs/genNPrimes.js';
+import multiplyInto2dArray from './../libs/multiplyInto2dArray.js';
+import InputForm from './InputForm.jsx';
+import OutputTable from './OutputTable.jsx';
+
+const renderTest = '';
 
 export default class App extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      outputArray: []
+    };
+    this.handleComponentSubmit = this.handleComponentSubmit.bind(this);
+  }
+
+  handleComponentSubmit(e){
+    var primeArray = genNPrimes(e.number);
+    this.setState({outputArray: multiplyInto2dArray(primeArray)});
   }
   
   render() {
     return (
       <div>
-		Hello World!
+        <InputForm renderTest={renderTest} onComponentSubmit={this.handleComponentSubmit} />
+        <OutputTable renderTest={renderTest} data={this.state.outputArray} />
       </div>
     );
   }
